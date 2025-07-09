3b350081350676c67f1156f74701836be40dfe7d
jxd8y <jxvzd@proton.me>
2025-07-09 10:08:40 -0700

    changed Op node color

--- include/BinaryTree.h
+++ include/BinaryTree.h
@@ -11,7 +11,7 @@ const float TREE_MIN_ZOOM_SCALE = 0.01f;
 const float TREE_MAX_ZOOM_SCALE = 5.0f;
 const float TREE_ZOOM_SCALE_INC_SPEED = 0.1f;
 
-const auto OP_NODE_COL = IM_COL32(255,0 , 40, 255);
+const auto OP_NODE_COL = IM_COL32(0,0 , 255, 200);
 const auto VAL_NODE_COL = IM_COL32(255, 0, 0, 255);
 const auto TXT_COL = IM_COL32(255, 255, 255, 255);
 
